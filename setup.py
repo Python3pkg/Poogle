5e29338d0e64866462c29b1d373601db6cfec072
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-30 17:48:57 -0500

    v0.1.0 pre-alpha build

--- setup.py
+++ setup.py
@@ -1,19 +1,39 @@
 from setuptools import setup, find_packages
 
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
     name='Poogle',
     version='0.1.0',
-    description='Google search results scraper for Python',
-    long_description='Google search results scraper for Python.',
+    description='Poogle is a Google scraping library and command line utility for Python.',
+    long_description=readme(),
     author='Makoto Fujimoto',
     author_email='makoto@makoto.io',
     url='https://github.com/FujiMakoto/Poogle',
     license='MIT',
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
+
+        'Environment :: Console',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
     ],
+    keywords=['google', 'web search', 'search engine'],
     packages=find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': [
