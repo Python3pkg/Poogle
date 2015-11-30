31dbbaed69f4baf95f8228e27f255fb994747b7c
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-29 22:47:29 -0500

    PoogleTestCase

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -8,20 +8,51 @@ import poogle
 from poogle import containers
 
 
-class PoogleResultsPageTestCase(unittest.TestCase):
+class PoogleBaseTestCase(unittest.TestCase):
 
     @mock.patch('poogle.Poogle')
     def setUp(self, mock_poogle):
 
         self.html_dir = os.path.join(os.path.dirname(__file__), 'html')
         with open(os.path.join(self.html_dir, 'test.html'), "r") as f:
-            html = f.read()
+            self.html = f.read()
 
         self.mock_poogle = mock_poogle
         mock_poogle.strict = False
 
-        self.soup = BeautifulSoup(html, 'html.parser')
+        self.soup = BeautifulSoup(self.html, 'html.parser')
         self.results_page = containers.PoogleResultsPage(mock_poogle, self.soup)
+
+
+class PoogleTestCase(PoogleBaseTestCase):
+
+    @mock.patch('poogle.requests.get')
+    def test_poogle_attributes(self, mock_get):
+
+        mock_get_response = mock.Mock()
+        mock_get_response.content = self.html
+        mock_get.return_value = mock_get_response
+
+        obj = poogle.Poogle('test', 10)
+
+        self.assertEqual(obj._query, 'test')
+        self.assertEqual(obj.query, 'test')
+        self.assertEqual(obj._min_results, 10)
+        self.assertEqual(obj._query_count, 1)
+        self.assertEqual(obj.total_results, 2390000000)
+        self.assertEqual(obj._current, 20)
+
+        self.assertIsInstance(obj.last, containers.PoogleResultsPage)
+        self.assertEqual(obj._results[0][0], 0)
+        self.assertIsInstance(obj._results[0][1], containers.PoogleResultsPage)
+        self.assertEqual(len(obj._results[0][1]), 20)
+
+        self.assertFalse(obj.strict)
+        self.assertFalse(obj._lazy)
+        self.assertEqual(obj._pause, 0.5)
+
+
+class PoogleResultsTestCase(PoogleBaseTestCase):
 
     def test_results_page_container_attributes(self):
         self.assertIs(self.results_page._poogle, self.mock_poogle)
